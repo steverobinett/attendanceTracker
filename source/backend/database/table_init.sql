c060658dea97760e6c50a923bd0b65f9cea85cef
Louis Solana <louis.solana@gmail.com>
2018-03-07 14:18:30 -0700

    Initial SQL table init file.

---
+++ source/backend/database/table_init.sql
@@ -0,0 +1,47 @@
+/* Groups */
+CREATE TABLE Groups
+(
+
+);
+
+
+/* Users */
+CREATE TABLE Users
+(
+
+);
+
+
+/* Attendance_Lists */
+CREATE TABLE User_Attendance_Lists
+(
+
+);
+
+
+/* Classes */
+CREATE TABLE Classes
+(
+
+);
+
+
+/* Class_Lists */
+CREATE TABLE Class_Lists
+(
+
+);
+
+
+/* Class_Schedules */
+CREATE TABLE Class_Schedules
+(
+
+);
+
+
+/* Class_Notes */
+CREATE TABLE Class_Notes
+(
+    
+);

b5cd6a539c46ed11c71b7fb2c22a91057d321e36
Louis Solana <louis.solana@gmail.com>
2018-03-11 20:13:54 -0600

    Fixed spelling and syntax, adjusted values to reflect use of ENUM type.

--- source/backend/database/table_init.sql
+++ source/backend/database/table_init.sql
@@ -1,13 +1,11 @@
-
 /* Users */
 CREATE TABLE Users
 (
     user_id INT AUTO_INCREMENT,
     user_type ENUM('Admin', 'Executive', 'Faculty', 'Student'),
-    user_name VARCHAR,
-    user_pass VARCHAR,
+    user_name VARCHAR(30),
+    user_pass VARCHAR(30),
     PRIMARY KEY (user_id)
-
 );
 
 
@@ -15,8 +13,8 @@ CREATE TABLE Users
 CREATE TABLE Classes
 (
     class_id INT AUTO_INCREMENT,
-    class_name VARCHAR,
-    class_code VARCHAR,
+    class_name VARCHAR(60),
+    class_code VARCHAR(10),
     PRIMARY KEY (class_id)
 );
 
@@ -26,24 +24,23 @@ CREATE TABLE User_Attendance
 (
     user_id INT,
     class_id INT,
-    att_date DATE,
+    att_date DATE NOT NULL,
     att_state ENUM ('Present', 'Late', 'Absent', 'Excused'),
     PRIMARY KEY (att_date),
-    FOREIGN KEY (user_id) REFEERENCES Users(user_id),
-    FOREIGN KEY (class_id) REFEERENCES Classes(class_id)
+    FOREIGN KEY (user_id) REFERENCES Users (user_id),
+    FOREIGN KEY (class_id) REFERENCES Classes (class_id)
 );
 
 
 /* Class_Lists */
 CREATE TABLE Class_Lists
 (
+    list_id INT NOT NULL AUTO_INCREMENT,
     user_id INT,
     class_id INT,
-    instructor VARCHAR,
-    PRIMARY KEY (instructor),
-    FOREIGN KEY (user_id) REFEERENCES Users(user_id)
-    FOREIGN KEY (class_id) REFEERENCES Classes(class_id)
-    
+    PRIMARY KEY (list_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (class_id) REFERENCES Classes(class_id)
 );
 
 
@@ -51,11 +48,11 @@ CREATE TABLE Class_Lists
 CREATE TABLE Class_Schedules
 (
     class_id INT,
-    weekday ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
+    weekday ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'),
     class_start TIME,
     class_end TIME,
     PRIMARY KEY (weekday, class_start),
-    FOREIGN KEY (class_id) REFEERENCES Classes(class_id)
+    FOREIGN KEY (class_id) REFERENCES Classes(class_id)
 );
 
 
@@ -63,8 +60,8 @@ CREATE TABLE Class_Schedules
 CREATE TABLE Class_Notes
 (
     class_id INT,
-    class_note VARCHAR,
+    class_note VARCHAR(255),
     class_note_date DATE,
     PRIMARY KEY (class_note_date),
-    FOREIGN KEY (class_id) REFEERENCES Classes(class_id)
+    FOREIGN KEY (class_id) REFERENCES Classes(class_id)
 );
